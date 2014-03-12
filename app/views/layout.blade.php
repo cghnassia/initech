b6131634f6d196b1e6cffd920138bc1d5489c1be
Clément GHNASSIA <clement.ghnassia@gmail.com>
2014-03-12 23:44:11 +0100

    Set up Laravel Framework Downloaded and installed laravel framework Created a new bundle for the site (site) Configured the first default route ('/') Configured the first default controller (HomeController) Created the first views linked to the controller layout and index)

---
+++ app/views/layout.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Initech project for internship application">
+    <meta name="author" content="Clément GHNASSIA">
+    <link rel="shortcut icon" href="img/initech.png">
+
+    <title>Initech project</title>
+
+    <!-- Bootstrap core CSS -->
+    {{ HTML::style('css/bootstrap.min.css') }}
+
+    <!-- Custom styles for this template -->
+    {{ HTML::style('css/jumbotron.css') }}
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Initech</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <form class="navbar-form navbar-right" role="form">
+            <div class="form-group">
+              <input type="text" placeholder="Email" class="form-control">
+            </div>
+            <div class="form-group">
+              <input type="password" placeholder="Password" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-success">Sign in</button>
+          </form>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </div>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        @yield('jumbotron')
+      </div>
+    </div>
+
+    <div class="container">
+      @yield('container')
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    {{ HTML::script('js/jquery.min.js') }}
+    {{ HTML::script('js/bootstrap.min.js') }}
+  </body>
+</html>
